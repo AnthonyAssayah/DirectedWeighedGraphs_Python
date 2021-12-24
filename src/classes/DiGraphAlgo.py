b1f3e8c66cbb2257c7f9751d475542fe32ca2044
annapinchuk <76442355+annapinchuk@users.noreply.github.com>
2021-12-24 09:54:29 +0200

    BFS with printing,  not in structure

--- src/classes/DiGraphAlgo.py
+++ src/classes/DiGraphAlgo.py
@@ -176,6 +176,35 @@ class DiGraphAlgo(GraphAlgoInterface):
     #
     #     return tsp_path;
     # }
+    def BFS(self, s):
+
+        # Mark all the vertices as not visited
+        visited = [False] * (max(self.DWG) + 1)
+
+        # Create a queue for BFS
+        queue = []
+
+        # Mark the source node as
+        # visited and enqueue it
+        queue.append(s)
+        visited[s] = True
+
+        while queue:
+
+            # Dequeue a vertex from
+            # queue and print it
+            s = queue.pop(0)
+            print(s, end=" ")
+
+            # Get all adjacent vertices of the
+            # dequeued vertex s. If a adjacent
+            # has not been visited, then mark it
+            # visited and enqueue it
+            for i in self.DWG[s]:
+                if visited[i] == False:
+                    queue.append(i)
+                    visited[i] = True
+
 
     def centerPoint(self) -> (int, float):
         pass
